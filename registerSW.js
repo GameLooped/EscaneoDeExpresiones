971b24fb87c29167b918cd4c4d07a7d9afb50afd
GameLooped <juandiegoalarconpongo@gmail.com>
2026-05-31 23:09:52 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/EscaneoDeExpresiones/sw.js', { scope: '/EscaneoDeExpresiones/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
